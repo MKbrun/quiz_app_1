2a33369d5b379fb8a9d6b56a7edcba784c238f3b
mkbrun <matskbrun@hotmail.com>
2024-09-19 23:08:12 +0200

    created for loop to iterate answers into map

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app_1/data/questions.dart';
 import 'package:quiz_app_1/questions_screen.dart';
 import 'package:quiz_app_1/start_screen.dart';
+import 'package:quiz_app_1/results_screen.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -11,7 +13,7 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  final List<String> selectedAnswers = [];
+  List<String> selectedAnswers = [];
   var activeScreen = 'start-screen';
 
   void switchScreen() {
@@ -22,6 +24,13 @@ class _QuizState extends State<Quiz> {
 
   void chooseAnswers(String answer) {
     selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        selectedAnswers = [];
+        activeScreen = 'results-screen';
+      });
+    }
   }
 
   @override
@@ -31,6 +40,12 @@ class _QuizState extends State<Quiz> {
     if (activeScreen == 'question-screen') {
       screenWidget = QuestionsScreen(
         onSelectAnswer: chooseAnswers,
+      );
+    }
+
+    if (activeScreen == 'results-screen') {
+      screenWidget = ResultsScreen(
+        chosenAnswers: selectedAnswers,
       );
     }
 
